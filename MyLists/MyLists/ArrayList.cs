cb338ffb3f7c3a734e2d9d5ef624fd1d7a665c2f
Maxim Bespalov <i.maksim_01@mail.ru>
2022-03-19 21:04:37 +0300

    add WriteArray, UpSize

--- MyLists/MyLists/ArrayList.cs
+++ MyLists/MyLists/ArrayList.cs
@@ -8,5 +8,34 @@ namespace MyLists
 {
     class ArrayList
     {
+        public int Length { get; private set; }
+
+        private int[] _array;
+
+        public ArrayList()
+        {
+            _array = new int[10];
+            Length = 0;
+        }
+
+        public void WriteArray()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                Console.Write($"{_array[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        private void UpSize()
+        {
+            int newLength = (int)(_array.Length * 1.5d + 1);
+            int[] newArray = new int[newLength];
+            for (int i = 0; i < _array.Length; i++)
+            {
+                newArray[i] = _array[i];
+            }
+            _array = newArray;
+        }
     }
 }
