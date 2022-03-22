f4ecd599b2b179e7bf7366eaf209890602551f4a
Maxim Bespalov <i.maksim_01@mail.ru>
2022-03-22 14:19:09 +0300

    Add first test AddToEndTest()

--- MyClass/MyLists.Tests/ArrayListTest.cs
+++ MyClass/MyLists.Tests/ArrayListTest.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections;
 using NUnit.Framework;
+using MyLists.Tests.ArrayListTestsSources;
 
 namespace MyLists.Tests
 {
     class ArrayListTest
     {
+        [TestCaseSource(typeof(AddToEndTestSource))]
+        public void AddToEndTest(int value, ArrayList list, ArrayList expectedList)
+        {
+            ArrayList actualList = list;
+            actualList.AddToEnd(value);
+
+            Assert.AreEqual(expectedList, actualList);
+        }
     }
 }
---
+++ MyClass/MyLists.Tests/ArrayListTestsSources/AddToEndTestSource.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLists.Tests.ArrayListTestsSources
+{
+    internal class AddToEndTestSource : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            int value = 5;
+            ArrayList list = new ArrayList(new int[] { 1, 2, 3 });
+            ArrayList expectedList = new ArrayList(new int[] { 1, 2, 3, 5 });
+            yield return new object[] { value, list, expectedList };
+        }
+    }
+}
