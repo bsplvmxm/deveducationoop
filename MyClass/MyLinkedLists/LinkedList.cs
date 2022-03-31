37dc27a4f5e0044b44222641e50f5602c6ed8e20
Maxim Bespalov <i.maksim_01@mail.ru>
2022-03-31 12:38:58 +0300

    add Node, +delete by index, addtoend, overrides

--- MyClass/MyLinkedLists/LinkedList.cs
+++ MyClass/MyLinkedLists/LinkedList.cs
@@ -4,6 +4,172 @@ namespace MyLinkedLists
 {
     public class LinkedList
     {
+        private Node _root;
 
+        private Node _tail;
+
+        public int this[int index]
+        {
+            get
+            {
+                Node crnt = _root;
+                for (int i = 1; i <= index; i++)
+                {
+                    crnt = crnt.Next;
+                }
+
+                return crnt.Value;
+            }
+
+            set
+            {
+
+            }
+        }
+
+        public int Length
+        {
+            get
+            {
+                int count = 0;
+                Node crnt = _root;
+
+                while (crnt != null)
+                {
+                    crnt = crnt.Next;
+                    count++;
+                }
+
+                return count;
+            }
+
+            private set
+            {
+
+            }
+        }
+
+        public LinkedList()
+        {
+            _root = null;
+            _tail = null;
+        }
+
+        public LinkedList(int value)
+        {
+            _root = new Node(value);
+            _tail = _root;
+        }
+
+        public LinkedList(int[] values)
+        {
+            if (values.Length == 0)
+            {
+                _root = null;
+                _tail = null;
+            }
+            else
+            {
+                _root = new Node(values[0]);
+                _tail = _root;
+
+                for (int i = 1; i < values.Length; i++)
+                {
+                    AddToEnd(values[i]);
+                }
+            }
+        }
+
+        public void AddToEnd(int value)
+        {
+            if (_root == null)
+            {
+                _root = new Node(value);
+                _tail = _root;
+            }
+            else
+            {
+                _tail.Next = new Node(value);
+                _tail = _tail.Next;
+            }
+        }
+
+        public void DeleteByIndex(int index)
+        {
+            if (index < 0 || index > Length - 1)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            else if (index == 0)
+            {
+                _root = _root.Next;
+            }
+            else
+            {
+                Node crnt = _root;
+                for (int i = 1; i < index; i++)
+                {
+                    crnt = crnt.Next;
+                }
+                crnt.Next = crnt.Next.Next;
+            }
+        }
+
+        public override string ToString()
+        {
+            string s = "";
+            Node crnt = _root;
+
+            while (crnt != null)
+            {
+                s += $"{crnt.Value} ";
+                crnt = crnt.Next;
+            }
+
+            return s;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is LinkedList))
+            {
+                return false;
+            }
+
+            LinkedList list = (LinkedList)obj;
+
+            if (list.Length != this.Length)
+            {
+                return false;
+            }
+
+            Node thisCrnt = this._root;
+            Node listCrnt = list._root;
+
+            while (thisCrnt != null)
+            {
+                if (thisCrnt.Value != listCrnt.Value)
+                {
+                    return false;
+                }
+
+                thisCrnt = thisCrnt.Next;
+                listCrnt = listCrnt.Next;
+            }
+
+            return true;
+        }
+
+        private Node GetNodeByIndex(int index)
+        {
+            Node crnt = _root;
+
+            for (int i = 1; i <= index; i++)
+            {
+                crnt = crnt.Next;
+            }
+
+            return crnt;
+        }
     }
 }
---
+++ MyClass/MyLinkedLists/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyLinkedLists
+{
+    public class Node
+    {
+        public int Value { get; set; }
+
+        public Node Next { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+            Next = null;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
